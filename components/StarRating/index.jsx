b89397bf4f320350c3f35b88a56e0007dc97b151
DevXprite <devXprite@gmail.com>
2022-12-09 07:05:47 +0530

    Update index.jsx

--- components/StarRating/index.jsx
+++ components/StarRating/index.jsx
@@ -10,7 +10,7 @@ export default function StarRating(props) {
         rating={props.rating}
         numberOfStars={props.TotalStars || 10}
         name="rating"
-        starDimension="22px"
+        starDimension="20px"
         starRatedColor="#f1c40f"
         starEmptyColor="#5c5c5c"
         starSpacing="2px"
