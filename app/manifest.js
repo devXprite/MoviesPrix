982394e09011630e16bf5e2496e85e7ae36d9d52
devxprite <devxprite@gmail.com>
2023-11-27 20:12:29 +0530

    fix: Update manifest.js

--- app/manifest.js
+++ app/manifest.js
@@ -11,15 +11,21 @@ export default function manifest() {
         "icons": [
             {
                 "src": "/android-chrome-192x192.png",
+                "sizes": "144x144",
+                "type": "image/png",
+                "purpose": "any maskable"
+            },
+            {
+                "src": "/android-chrome-192x192.png",
                 "sizes": "192x192",
                 "type": "image/png",
-                "purpose": "maskable"
+                "purpose": "any maskable"
             },
             {
                 "src": "/android-chrome-512x512.png",
                 "sizes": "512x512",
                 "type": "image/png",
-                "purpose": "maskable"
+                "purpose": "any maskable"
             }
         ]
     }
